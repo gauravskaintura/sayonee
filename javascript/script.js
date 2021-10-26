24f60617987dc8e809a9c3f84fb45986c564536a
manshad <manshadchangampalli010@gmail.com>
2021-10-26 13:17:31 +0530

    the social media icon hover effect is added

--- javascript/script.js
+++ javascript/script.js
@@ -10,9 +10,42 @@ function scrollFunction() {
 }
 
 
-
+let facebook = document.querySelector('#facebook')
+let instagram = document.querySelector('#instagram')
+let twitter = document.querySelector('#twitter')
+let sm_name = document.querySelector('.socialmedia-name')
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('#navbar');
+
+// facebook hover animation 
+facebook.addEventListener('mouseover',()=>{
+    sm_name.innerHTML="facebook"
+    // sm_name.style.color="blue"
+    sm_name.style.opacity = "1"
+})
+facebook.addEventListener('mouseout',()=>{
+    sm_name.style.opacity = "0"
+})
+
+// instagram hover animation 
+instagram.addEventListener('mouseover',()=>{
+  sm_name.innerHTML="instagram"
+  sm_name.style.opacity = "1"
+})
+instagram.addEventListener('mouseout',()=>{
+  sm_name.style.opacity = "0"
+})
+
+// twitter hover animation 
+twitter.addEventListener('mouseover',()=>{
+  sm_name.innerHTML="twitter"
+  sm_name.style.opacity = "1"
+  // sm_name.style.color="blue"
+
+})
+twitter.addEventListener('mouseout',()=>{
+  sm_name.style.opacity = "0"
+})
 
 menu.onclick = () =>{
 
